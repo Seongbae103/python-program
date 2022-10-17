b89e07bbff6b493c8ba0321e61e8c36bade1bcdb
Seongbae103 <seongbae103@gmail.com>
2022-10-17 16:59:27 +0900

    10-17

---
+++ encapsulation/bmi.py
@@ -0,0 +1,52 @@
+'''
+키와 몸무게를 입력받아서 비만도를 측정하는 프로그램을 완성하시오.
+BMI 지수를 구하는 공식은 다음과 같다.
+BMI지수 = 몸무게(70kg) / (키(1.7m) * 키(1.7m))
+
+BMI 지수에 따른 결과는 다음과 같다.
+고도 비만 : 35 이상
+중(重)도 비만 (2단계 비만) : 30 - 34.9
+경도 비만 (1단계 비만) : 25 - 29.9
+과체중 : 23 - 24.9
+정상 : 18.5 - 22.9
+저체중 : 18.5 미만
+
+이름, 키, 몸무게를 입력받으면 다음과 같이 출력되도록 하시오.
+
+***************************
+이름 키(cm) 몸무게(kg) 비만도
+***************************
+홍길동 170 70 정상
+***************************
+'''
+class Bmi(object):
+    def __init__(self, name, h, w):
+        self.name = name
+        self.h = h
+        self.w = w
+    def bmi(self):
+        h = self.h
+        w = self.w
+        m = h/100
+        ast = "*"*27
+        bmi = w/(m*m)
+        if bmi >= 35:
+            biman = "고도 비만"
+        elif bmi >= 30:
+            biman = "중(重)도 비만 (2단계 비만)"
+        elif bmi >= 25:
+            biman = "경도 비만 (1단계 비만)"
+        elif bmi >= 23:
+            biman = "과체중"
+        elif bmi >= 18.5:
+            biman = "정상"
+        else:
+            biman = "저체중"
+        print(f'{ast} \n '이름', '키(cm)', '몸무게(kg)', '비만도' \n {ast} \n {naem}, {h}, {w}, {biman} \n {ast} ')
+
+if __name__=="__main__":
+    name = input("이름 : ")
+    h = int(input("키 : "))
+    w = int(input("체중 : "))
+    bmi = Bmi()
+    bmi.bmi()
