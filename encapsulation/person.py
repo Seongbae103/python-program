d5aa3eac3911ce313198430f2e290585721733e5
Seongbae103 <seongbae103@gmail.com>
2022-10-19 14:59:25 +0900

    10-19

--- encapsulation/person.py
+++ encapsulation/person.py
@@ -14,24 +14,25 @@ from audioop import add
 
 
 class Person:
-    def __inti__(self,name, ssn):
+    def __inti__(self,name, ssn, add):
         self.name = name
         self.ssn = ssn
         self.add = add
         self.age = 0
         self.gender = ""
+        self.add = add
 
     def gender_checker(self):
         year = int(self.ssn[:2]) # 0:2는 출생년도 판별 인덱스
         gender_checker = int(self.ssn[7]) #7은 성별판별 인덱스
         if gender_checker == 1 or gender_checker == 2:
-            self.birth = (year + 1900)
+            self.birth = year + 1900
             if gender_checker == 1:
                 self.gender = "남성"
             elif gender_checker == 2:
                 self.gender = "여성"
         elif gender_checker == 3 or gender_checker == 4:
-            self.birth = (year + 2000)
+            self.birth = year + 2000
             if gender_checker == 3:
                 self.gender = "남성"
             elif gender_checker == 4:
@@ -41,22 +42,23 @@ class Person:
 
     def set_age(self):
         current = 2022
-        birth = self.birth
-        self.age = current - birth + 1
+        self.age = current - self.birth + 1
     
     def print_person(self):
         print( f"### 자기소개서 ###"
             "\n ********************************"
-            "\n 이름 : {name}"
-            "\n 성별 : {self.gender}"
-            "\n 나이 : {self.age}"
-            "\n 주소 : {self.add}" 
+            f"\n 이름 : {self.name}"
+            f"\n 성별 : {self.gender}"
+            f"\n 나이 : {self.age}"
+            f"\n 주소 : {self.add}" 
             |"\n ********************************")
+
     @staticmethod
     def main():
         name = input("이름 : ")
         ssn = input("주민번호 : ")
-        person = Person(name, ssn)
+        add = input(" 주소 : ")
+        person = Person(name, ssn, add)
         person.gender_checker()
         person.set_age()
         person.print_person()     
